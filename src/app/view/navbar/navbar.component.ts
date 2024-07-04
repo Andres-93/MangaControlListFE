7b200fe7418cc4aa757b9414998055854f7f5ab4
Andres <andres.pgomez93@gmail.com>
2024-07-04 16:24:22 +0200

    cambio add

--- src/app/view/navbar/navbar.component.ts
+++ src/app/view/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { MangaService } from '../manga-list/manga.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +9,11 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router,private mangaService: MangaService) { }
 
 
-  irAddManga(){
+  irAddManga(){ 
+    this.mangaService. eliminarMangaEdicion();
     this.router.navigate(['/add-manga']);
   }
 
